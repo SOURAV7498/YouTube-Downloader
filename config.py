3f1c39b1e461f3ebe84148ef67d8a46f3d4ab6ed
SOURAV7498 <gouravbolange@gmail.com>
2025-10-25 19:18:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID", ))
-    API_HASH = os.getenv("API_HASH", '')
-    BOT_TOKEN = os.getenv("BOT_TOKEN", '')
+    API_ID = int(os.getenv("API_ID", '21140176'))
+    API_HASH = os.getenv("API_HASH", 'b081ec8da8cf5263a6593041c1ae2a3b')
+    BOT_TOKEN = os.getenv("BOT_TOKEN", '8348778650:AAFtt5gch26hxzsG-ihJ1lVF7IQCn6Weq5Y')
